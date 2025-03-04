295b08dcc3bbcc3c2f6ba797e9ef537145c7c5f2
ashif raza <razaashif395@gmail.com>
2025-03-04 11:33:33 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,25 @@
+//COLLECTION MAPS IN JAVA
+import java.util.Map;
+import java.util.HashMap;
 
-import java.util.*;
-import java.io.*;
-public class Main
-{
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter value of n");
-        int n = s.nextInt();
-        if (n >= 0 && n <= 9) {
-            System.out.println("1 digit");
-        } else if (n >= 0 && n <= 100) {
-            System.out.println("2 digit");
-        } else if (n >= 100 && n <= 999) {
-            System.out.println("3 digit");
-        } else {
-            System.out.println("Invalid Number");
+class Main {
+    public static void main(String[]args){
+        Map<String,Integer> country = new HashMap<>();//Creating Empty Map
+        System.out.println("COUMTRIES DETAIL");
+        System.out.println("______________");
+
+        country.put("INDIA",1);
+        country.put("CHINA",2);
+        country.put("NORTH KOREA",7);
+
+        System.out.println("COUMTRIES"+country);
+
+        for(Map.Entry<String,Integer>entry : country.entrySet()){
+            System.out.println("COUNTRIES :"+entry.getKey()+entry.getValue());
         }
+
+
+
+
     }
 }

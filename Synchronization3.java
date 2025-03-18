bc4e2de4d3949dfb0667457df070324815afeda7
ashif raza <razaashif395@gmail.com>
2025-03-18 15:03:14 +0530

    Add files via upload

---
+++ Synchronization3.java
@@ -0,0 +1,94 @@
+//book details
+import java.sql.SQLOutput;
+import java.util.Scanner;
+class Details1{
+    void head(){
+        System.out.println("BOOK DETAILS");
+        System.out.println("------------------");
+
+    }
+    synchronized void library(String detail){
+        System.out.print("["+detail);
+
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            System.out.println(e);
+        }
+        System.out.println("]");
+    }
+}
+class MyThread2 extends Thread {
+    Details print_bk;
+    String det;
+
+    MyThread2(Details print_bk, String det) {
+        this.print_bk=print_bk;
+        this.det = det;
+
+    }
+    public void run(){
+        print_bk.library(det);
+    }
+}
+public class Synchronization3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Do you want to add book:(yes/no)");
+
+            String input = s.nextLine();
+            if(input.equals("no")|| input.equals("n")){
+                System.out.println("Exiting....");
+                break;
+            }
+
+
+
+            System.out.println("Enter Book Name:");
+            String book_name = s.nextLine();
+            System.out.println("Enter Book Price");
+            String book_price = s.nextLine();
+            System.out.println("Enter Book Publish Date");
+            String book_publish_date = s.nextLine();
+            System.out.println("Enter Book Author Name");
+            String book_author = s.nextLine();
+
+            Details d = new Details();
+            d.head();
+
+
+            //MyThread1 b1 = new MyThread1(d,"Book Details");
+            //MyThread1 b2 = new MyThread1(d,"------------------------------------");
+            MyThread1 b3 = new MyThread1(d, "Book Name:" + book_name);
+            MyThread1 b4 = new MyThread1(d, "Book Author:" + book_author);
+            MyThread1 b5 = new MyThread1(d, "Book Publish Date:" + book_publish_date);
+            MyThread1 b6 = new MyThread1(d, "Book Price:" + book_price);
+
+            //b1.start();
+            //b2.start();
+            b3.start();
+            b4.start();
+            b5.start();
+            b6.start();
+
+
+            //Adding new books data to Threads
+
+            try{
+                b3.join();
+                b4.join();
+                b5.join();
+                b6.join();
+
+            }catch(InterruptedException e){
+                e.printStackTrace();
+
+
+            }
+
+        }
+        s.close();
+    }
+}

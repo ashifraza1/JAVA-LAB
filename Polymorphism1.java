e07d0032c93b84afe3db5b3c16a27c74a5594979
ashif raza <razaashif395@gmail.com>
2025-03-18 13:29:58 +0530

    Add files via upload

---
+++ Polymorphism1.java
@@ -0,0 +1,19 @@
+class MathOperations {
+    int add(int a, int b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+public class Polymorphism1 {
+    public static void main(String[] args) {
+        MathOperations obj = new MathOperations();
+        System.out.println(obj.add(5, 10));
+        System.out.println(obj.add(5, 10, 15));
+        System.out.println(obj.add(5.5, 2.3));
+    }
+}
